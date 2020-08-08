244689255487de92cd44a5fa20275be719ee6444
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-08 21:29:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,7 +32,7 @@ def get_english_meaning(file, japenese_emoji)
   message = "Sorry, that emoticon was not found"
   library.each do |value, meaning|
     if library[value][:japanese] == japenese_emoji
-      return library[value][:english]
+      return library[:english]
     end
   end
   return message
