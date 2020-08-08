118ce50be6e65f970ea99ddcedbe64eedd5587ca
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-08 20:45:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ def get_japanese_emoticon(file,english_emoji)
       return library[value][:japanese]
     end
  end
- message
+ return message
 end
 
 def get_english_meaning(file, japenese_emoji)
